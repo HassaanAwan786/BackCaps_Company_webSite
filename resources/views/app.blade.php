1afbddd6794624bd05b8e1ad95cb5701886d407b
ibnay hussain <zainishaaah@gmail.com>
2026-05-25 15:52:42 +0500

    update font style to font rubik

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -51,7 +51,7 @@
         <!-- Fonts -->
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Unbounded:wght@200;300;400;500;600;700;800;900&family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+        <link href="https://fonts.googleapis.com/css2?family=Instrument+Sans:ital,wght@0,400..700;1,400..700&family=Rubik:ital,wght@0,300..900;1,300..900&display=swap" rel="stylesheet">
 
         <!-- Scripts -->
         @routes
